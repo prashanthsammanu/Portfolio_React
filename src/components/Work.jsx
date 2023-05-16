5cc3f896f26aac0d8b74511f6b2eb08d766b56c4
prashanthsammanu <prashanthsammanu@gmail.com>
2023-05-16 05:53:24 -0400

    feat: added a project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import Rawlab from "../assets/Rawlab.jpeg";
-import Uber from "../assets/uber1.avif";
-import Viral from "../assets/viral.png";
 import Raw from "../assets/raw.png";
 import viz from "../assets/viz.png";
+import FaceParsing from "../assets/FP2.png";
 
 const Work = () => {
   return (
@@ -58,6 +56,29 @@ const Work = () => {
                   </button>
                 </a>
                 <a href="https://github.com/prashanthsammanu/Uber-Movement-VisuaIization">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${FaceParsing})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Object Segmentation and Face Parsing
+              </span>
+              <div className="pt-8 text-center">
+                {/* <a href="https://rawlab.org/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Website
+                  </button>
+                </a> */}
+                <a href="https://github.com/shalini-ganni/CCN-Group6">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
